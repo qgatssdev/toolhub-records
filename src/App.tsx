a67579910e8950dc94fd92f36a4d540b2429b815
Uche Omodu <chivoomodu@gmail.com>
2022-10-20 23:14:33 +0100

    Create routes and add css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import "./App.css";
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Leaderboard from "./pages/Leaderboard/Leaderboard";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Router>
+        <Routes>
+          <Route path="/Leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
