aaf00ce8ef1ab60ebad6e9f56f81af15d0af551f
Uche Omodu <chivoomodu@gmail.com>
2022-10-21 17:45:16 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import "./App.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Leaderboard from "./pages/Leaderboard/Leaderboard";
 import "./App.scss";
+import Home from "./pages/Home/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/Leaderboard" element={<Leaderboard />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
