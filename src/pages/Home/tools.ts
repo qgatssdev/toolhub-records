778d881347500ded5b0a794365b546367a0f18d6
Uche Omodu <chivoomodu@gmail.com>
2022-10-27 22:50:49 +0100

    Build tools mockdata

---
+++ src/pages/Home/tools.ts
@@ -0,0 +1,47 @@
+export const tools = [
+  {
+    id: 1,
+    toolName: "XTools Edit Counter",
+    Authors: "MusikAnimal, Samwilson...",
+    description:
+      "Legendary editcounter and analyser of user contributions. Fast and feature-rich.",
+    logo: "tool",
+  },
+  {
+    id: 2,
+    toolName: "CropTool",
+    Authors: "Dan Michael O. Heggø",
+    description:
+      "Tool for cropping images at Wikimedia Commons and other Wikimedia sites.",
+    logo: "globe",
+  },
+  {
+    id: 3,
+    toolName: "Find duplicate items",
+    Authors: "Magnus Manske",
+    description: "Find duplicate items on Wikidata, via labels/aliases.",
+    logo: "tool",
+  },
+  {
+    id: 4,
+    toolName: "diffedit",
+    Authors: "Jon Harald Søby",
+    description: "Enables editing directly from viewing a diff.",
+    logo: "scroll",
+  },
+  {
+    id: 5,
+    toolName: "Listeria",
+    Authors: "Magnus Manske",
+    description:
+      "Generates lists on Wikipedias, based on Wikidata queries. Daily updates.",
+    logo: "robot",
+  },
+  {
+    id: 6,
+    toolName: "Wudele",
+    Authors: "JeanFred.",
+    description: "Schedule an event or make a poll",
+    logo: "globe",
+  },
+];
