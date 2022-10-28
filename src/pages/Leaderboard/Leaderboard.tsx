b86d0c0ba61afcea991c932053f6e017cd8445d7
Uche Omodu <chivoomodu@gmail.com>
2022-10-28 14:44:49 +0100

    Build logic for ranking and leaderboard

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,6 +1,8 @@
 import Header from "../../components/Header/Header";
 import "./Leaderboard.scss";
 import { DateRangePicker } from "rsuite";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import { data } from "./Data";
 
 const Leaderboard = () => {
   return (
@@ -12,7 +14,7 @@ const Leaderboard = () => {
         <div className="heading__div1">
           <p className="heading__div1__leaderboard">Leaderboard</p>
           <p className="heading__div1__details">
-            Monitor overall performance of all students
+            Monitor overall performance of all Tools
           </p>
         </div>
         <div className="heading__div2">
@@ -23,6 +25,33 @@ const Leaderboard = () => {
             format="dd-MM-yyyy"
           />
         </div>
+      </section>
+      <section className="Section__Leader">
+        <Table className="Leader__table">
+          <Thead>
+            <Tr className="Tr">
+              <Th className="Th">Rank</Th>
+              <Th className="Th">Tool</Th>
+              <Th className="Th">Tool ID</Th>
+              <Th className="Th">Points</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data
+              .sort((a, b) => (a.points > b.points ? -1 : 1))
+              .map((data, index) => {
+                const rank = index + 1;
+                return (
+                  <Tr className="Tr" key={data.toolID}>
+                    <Td className="Td">{rank}</Td>
+                    <Td className="Td">{data.toolName}</Td>
+                    <Td className="Td">{data.toolID}</Td>
+                    <Td className="Td">{data.points}</Td>
+                  </Tr>
+                );
+              })}
+          </Tbody>
+        </Table>
       </section>
     </div>
   );
