8544112acca4177052e320ef9e9ef45bee735e56
Uche Omodu <chivoomodu@gmail.com>
2022-10-28 14:44:19 +0100

    build mock data

---
+++ src/pages/Leaderboard/Data.ts
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    toolName: "Crop Tool",
+    toolRank: 4,
+    toolID: "200",
+    points: 400,
+  },
+  {
+    toolName: "locator-tool",
+    toolRank: 9,
+    toolID: "43",
+    points: 20,
+  },
+  {
+    toolName: "Wikidata Todo",
+    toolRank: 10,
+    toolID: "77",
+    points: 500,
+  },
+  {
+    toolName: "ShExStatements",
+    toolRank: 2,
+    toolID: "299",
+    points: 10,
+  },
+  {
+    toolName: "QuickStatements",
+    toolRank: 3,
+    toolID: "488",
+    points: 1000,
+  },
+  {
+    toolName: "Ranker",
+    toolRank: 5,
+    toolID: "299",
+    points: 402,
+  },
+  {
+    toolName: "Name to Q",
+    toolRank: 4,
+    toolID: "388",
+    points: 204,
+  },
+  {
+    toolName: "Deciptor",
+    toolRank: 6,
+    toolID: "13",
+    points: 501,
+  },
+  {
+    toolName: "Listeria",
+    toolRank: 7,
+    toolID: "34",
+    points: 1,
+  },
+  {
+    toolName: "Quarry",
+    toolRank: 8,
+    toolID: "9",
+    points: 33,
+  },
+];
