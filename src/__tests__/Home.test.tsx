1ddd7b033be74903003ba289ffbc2f38552507b6
Uche Omodu <chivoomodu@gmail.com>
2022-11-04 00:00:25 +0100

    Add test files

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import Leaderboard from "../pages/Leaderboard/Leaderboard";
 
 test("full app rendering/navigating", async () => {
