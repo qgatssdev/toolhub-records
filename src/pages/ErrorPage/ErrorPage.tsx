6a45ec555fd3bd4bc1486cc3dbe1cff7ecaf054d
Uche Omodu <chivoomodu@gmail.com>
2022-11-03 23:57:44 +0100

    Add error page

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ErorrPage.scss";
+
+const ErrorPage = () => {
+  return (
+    <div id="main">
+      <div className="fof">
+        <h1>Oops, Page not found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
